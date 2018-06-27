95165e85f2642138440490a91518a83cf1bf5d5a
Eldar Udovicic <eud@dis-play.dk>
2018-06-27 09:46:12 +0200

    Add project files.

---
+++ Events.Tac.Local.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Events.Tac.Local.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Events.Tac.Local/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Events.Tac.Local.MvcApplication" Language="C#" %>
