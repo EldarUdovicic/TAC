83d41ea8c05c87cf9290dd68bb07aa7c863026a3
EldarUdovicic <eud@dis-play.dk>
2018-06-28 11:59:16 +0200

    include all changes

---
+++ Events.Tac.Local.Tests/Test1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Events.Tac.Local.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
