aa032574f15b6d39746970ca59300b845168451f
EldarUdovicic <eud@dis-play.dk>
2018-08-13 11:39:10 +0200

    Hele modul 5 færdigt.

---
+++ Events.Tac.Local/Controllers/NavigationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+
+namespace Events.Tac.Local.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
